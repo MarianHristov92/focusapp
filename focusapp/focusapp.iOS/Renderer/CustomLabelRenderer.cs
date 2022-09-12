61dc93cbaf364300d1589f7926bb109311e7ee72
Marian Hristov <marianhrstv@yahoo.com>
2022-09-12 11:10:16 +0300

    IOS renderer uploaded

---
+++ focusapp/focusapp.iOS/Renderer/CustomLabelRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using focusapp.iOS.Renderer;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace focusapp.iOS.Renderer
+{
+    [Preserve(AllMembers = true)]
+	public class CustomLabelRenderer : LabelRenderer
+	{
+		#region ViewRenderer Overrides
+		UIColor originalColor;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+			if (Control != null)
+				originalColor = Control.BackgroundColor;
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+		}
+
+		public override bool CanBecomeFocused => true;
+
+		public override void DidUpdateFocus(UIFocusUpdateContext context, UIFocusAnimationCoordinator coordinator)
+		{
+			base.DidUpdateFocus(context, coordinator);
+			if (Control.BackgroundColor == originalColor)
+				Control.BackgroundColor = UIColor.Red;
+			else
+				Control.BackgroundColor = originalColor;
+		}
+		#endregion
+	}
+}
---
+++ focusapp/focusapp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using focusapp.ViewModels;
+
+namespace focusapp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
